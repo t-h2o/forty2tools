0b7ad0740ab87ebc80878bd94e1fae24014ab1c7
|°_°| <grivel.theo@protonmail.com>
2023-02-22 04:40:42 +0100

    good practice: add quote for variables
    Double quote to prevent globbing and word splitting.
    https://www.shellcheck.net/wiki/SC2086

--- generate_header.sh
+++ generate_header.sh
@@ -7,13 +7,13 @@ help () {
 
 main () {
 	# Set a file name
-	file_name=$(printf "%s.h" ${1} | awk '{print tolower($0)}')
+	file_name=$(printf "%s.h" "${1}" | awk '{print tolower($0)}')
 
 	# Set to upper the argument and append _H
-	header_guard_name=$(printf "%s_H" ${1} | awk '{print toupper($0)}')
+	header_guard_name=$(printf "%s_H" "${1}" | awk '{print toupper($0)}')
 
 	#find protoypes using ctags
-	function_protoypes=$(ctags -Rx ${2} | grep 'function' | grep -v static | grep -vw main | sed -re 's,\s+, ,g' | cut -d ' ' -f 5-)
+	function_protoypes=$(ctags -Rx "${2}" | grep 'function' | grep -v static | grep -vw main | sed -re 's,\s+, ,g' | cut -d ' ' -f 5-)
 	# ctags -Rx
 	#	x to output to terminal in humanreadable form
 	#	R to recursivly search the directory
@@ -23,13 +23,13 @@ main () {
 	#	split the line into colums using ' ' as delim and take everything from the 5th column
 
 	# Print the header guard into a new file
-	printf "#ifndef %s\n" ${header_guard_name} > ${file_name}
-	printf "# define %s\n\n" ${header_guard_name} >> ${file_name}
+	printf "#ifndef %s\n" "${header_guard_name}" > "${file_name}"
+	printf "# define %s\n\n" "${header_guard_name}" >> "${file_name}"
 # | unexpand -a -t 2
-	printf '%s' "${function_protoypes}" | awk '{print $0 ";"}' | sed 's/ /:/1' | column -s ':' -t | unexpand -t2 >> ${file_name}
-	printf '\n' >> ${file_name}
+	printf '%s' "${function_protoypes}" | awk '{print $0 ";"}' | sed 's/ /:/1' | column -s ':' -t | unexpand -t2 >> "${file_name}"
+	printf '\n' >> "${file_name}"
 
-	printf "#endif /* %s */\n" ${header_guard_name} >> ${file_name}
+	printf "#endif /* %s */\n" "${header_guard_name}" >> "${file_name}"
 }
 
 # Check the number of arguments
@@ -38,4 +38,4 @@ if [ ${#} -ne 2 ]; then
 	exit 1
 fi
 
-main ${1} ${2}
+main "${1}" "${2}"
